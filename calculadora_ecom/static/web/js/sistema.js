ab2ea89f3151063b7117847422634f3e00a735f6
luise <luisenriquevelezsantos@gmail.com>
2021-11-03 13:31:40 -0500

    Selecctor

--- calculadora_ecom/static/web/js/sistema.js
+++ calculadora_ecom/static/web/js/sistema.js
@@ -2,10 +2,7 @@ $(document).ready(function (){
     var seleccionarFormula
     var objeto = {}
 
-    $(".seleccionarFormula").keyup(function () {
-        seleccionarFormula = $(this).val();
 
-    })
 
     $(".VP").keyup(function () {
         objeto['VP'] = $(this).val();
@@ -38,28 +35,34 @@ $(document).ready(function (){
 
     })
 
+    $('#seleccionarEvento').change(function() {
+        seleccionarFormula=$(this).val()
+        console.log(seleccionarFormula)
+
+        $('.abc').empty()
+
     switch (seleccionarFormula) {
-        case 1:
+        case "VP":
 
             var contendor=
-                $("<div class='container'>" +
+                $("<div class='container .abc'>" +
                     "<div class='row margin_TopDef'><label for='A'>A<input type='number' class='A' id='A'></label></div>" +
                     "<div class='row margin_TopDef'><label for='n'>N<input type='number' class='n' id='n'></label></div>"+
                     "<div class='row margin_TopDef'><label for='interes'>Interes<input type='number' class='interes' id='interes'></label></div>"+
                     "<div class='row margin_TopDef'><button class='calcular'>CALCULAR</button></div> "+
                     "</div>")
-                $('abc').append(contendor)
+                $('.abc').append(contendor)
 
             $("body").on('click','.calcular',function () {
             var convertJson = JSON.stringify(objeto)
             $.post("/home/anualidadVencida__VP/",{data:convertJson},function (data){
 
         })
-                break;
+
 
     })
     }
-
+    });
 
 
 
